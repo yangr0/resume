79695616ebc6d1e7a01a8eb3400558b2383e7154
raymond <ry25@illinois.edu>
2025-02-05 15:57:25 -0600

    fixed bad res

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [download here](https://github.com/yangr0/resume/blob/main/resume.pdf)
 
-<img src="https://github.com/yangr0/resume/blob/main/resume.png" width=100% />
+![resume-png](https://github.com/yangr0/resume/blob/main/resume.png)
--- convert.sh
+++ convert.sh
@@ -4,4 +4,4 @@
 
 cd "$(dirname "$0")"
 /usr/bin/pdflatex resume.tex
-/usr/bin/magick -density 300 resume.pdf -background white -alpha remove -alpha off resume.png
+/usr/bin/magick -density 300 resume.pdf -resize 50% -background white -alpha remove -alpha off resume.png
